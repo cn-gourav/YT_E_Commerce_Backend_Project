4daf6f8d5d2ef5bf734c941c5a4ba65691a44488
cn-gourav <mrgusain558@gmail.com>
2025-03-24 22:28:56 +0530

    ADDED : Some adjust and make middleware product models,user models, owner models, routers

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const ownersRouter = require("./routes/ownersRouter");  
+const usersRouter = require("./routes/usersRouter");  
+const productRouter = require("./routes/productRouter");
 
 const cookieParser = require("cookie-parser");
 const path = require("path");
+
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +16,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
-app.get("/",(req,res) =>{
-    res.send("try");
-})
+app.use("/owners", ownersRouter); 
+app.use("/users", usersRouter); 
+app.use("/products", productRouter); 
 
 app.listen(3000);
